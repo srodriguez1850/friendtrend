3942eb88c5b79e58049e29b5310710ff8d83ff7f
Prashant Jayannavar <paj3@illinois.edu>
2019-02-20 19:02:23 -0600

    refactor: Make modular, Stringify keys for plotly format

--- preprocess_data.py
+++ preprocess_data.py
@@ -19,13 +19,13 @@ for json_file in all_json_files:
         json_data["my_friend_id"] = json_file.split("/")[-2]
         all_json_data.append(json_data)
 
-print(len(all_json_data))
+# print(len(all_json_data))
 
 # filter out group conversations
 all_json_data_dm = list(filter(lambda x: x["thread_type"] == "Regular", all_json_data))
-print(len(all_json_data_dm))
+# print(len(all_json_data_dm))
 all_json_data_dm = list(filter(lambda x: x["participants"] != [{'name': 'Prashant Jayannavar'}], all_json_data_dm))
-print(len(all_json_data_dm))
+# print(len(all_json_data_dm))
 
 # processing
 def process(json):
@@ -56,38 +56,48 @@ def process(json):
 all_msgs = list(map(lambda x: process(x), all_json_data_dm))
 all_msgs = list(itertools.chain.from_iterable(all_msgs))
 
-print(len(all_msgs))
+# print(len(all_msgs))
 
 # grouping
 
-# group by year
-msgs_by_year = {}
-for year, group in itertools.groupby(sorted(all_msgs, key = lambda x: x["year"]), key = lambda x: x["year"]):
-    msgs_by_year[year] = list(group)
+def process_messages_by_year(all_msgs):
 
-# pp.pprint(msgs_by_year[2019])
+    # group by year
+    msgs_by_year = {}
+    for year, group in itertools.groupby(sorted(all_msgs, key = lambda x: x["year"]), key = lambda x: x["year"]):
+        msgs_by_year[year] = list(group)
 
-for key, value in msgs_by_year.items():
-    msgs_by_friend = {}
-    for my_friend_id, group in itertools.groupby(sorted(value, key = lambda x: x["my_friend_id"]), key = lambda x: x["my_friend_id"]):
-        msgs_by_friend[my_friend_id] = list(group)
-    msgs_by_year[key] = msgs_by_friend
+    # pp.pprint(msgs_by_year[2019])
 
-print("\n\n")
-pp.pprint(msgs_by_year[2019])
+    for key, value in msgs_by_year.items():
+        msgs_by_friend = {}
+        for my_friend_id, group in itertools.groupby(sorted(value, key = lambda x: x["my_friend_id"]), key = lambda x: x["my_friend_id"]):
+            msgs_by_friend[my_friend_id] = list(group)
+        msgs_by_year[key] = msgs_by_friend
 
-for key, value in msgs_by_year.items():
-    for key2, value2 in value.items():
-        msgs_by_year[key][key2] = len(value2)
+    # print("\n\n")
+    # pp.pprint(msgs_by_year[2019])
 
-print("\n\n")
-pp.pprint(msgs_by_year[2019])
+    for key, value in msgs_by_year.items():
+        for key2, value2 in value.items():
+            msgs_by_year[key][key2] = len(value2)
 
-for key, value in msgs_by_year.items():
-    msgs_by_year[key] = sorted(value.items(), key = lambda x: x[1], reverse = True)
+    # print("\n\n")
+    # pp.pprint(msgs_by_year[2019])
 
-print("\n\n")
-pp.pprint(msgs_by_year[2019])
+    for key, value in msgs_by_year.items():
+        msgs_by_year[key] = sorted(value.items(), key = lambda x: x[1], reverse = True)
 
-with open("viz_data_scratch.json", "w") as f:
-    json.dump(msgs_by_year, f)
+    # print("\n\n")
+    # pp.pprint(msgs_by_year[2019])
+
+    for old_key in msgs_by_year.keys():
+        new_key = str(old_key) + "-01-01"
+        msgs_by_year[new_key] = msgs_by_year.pop(old_key)
+
+    print(msgs_by_year.keys())
+
+    with open("messages_yearly_scratch.json", "w") as f:
+        json.dump(msgs_by_year, f)
+
+process_messages_by_year(all_msgs)
